71c2c156f0afca8387e055e27bc033aeaaf549c7
“Tanmay <tanmaymaity06@gmail.com>
2025-11-21 17:05:10 -0600

    updated files

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>reactadmin</span>
+      <span>© React Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
